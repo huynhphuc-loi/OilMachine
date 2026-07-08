635eafc82e0c73c8e1963cd0d31ee1aea180d2df
loihuynh <phucloi1011@gmail.com>
2026-07-08 17:26:31 +0700

    Fullscreen

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     w.setWindowFlags(Qt::FramelessWindowHint);
     w.setMinimumSize(1024, 600);
     w.setMaximumSize(1024, 600);
+    w.showFullScreen();
     w.show();
     return QApplication::exec();
 }
