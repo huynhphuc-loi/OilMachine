6134b2fb99160f7d8876f4e5a54c8acd4b900b3c
loihuynh <phucloi1011@gmail.com>
2026-07-08 17:19:21 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowFlags(Qt::FramelessWindowHint);
+    w.setMinimumSize(1024, 600);
+    w.setMaximumSize(1024, 600);
+    w.show();
+    return QApplication::exec();
+}
